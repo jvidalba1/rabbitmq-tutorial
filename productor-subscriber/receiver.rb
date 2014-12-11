27117fefa97abfae3dbda0b89f46cf3b2c08458e
Mateo Vidal <jvidalba@gmail.com>
2014-12-11 13:07:53 -0500

    adding infinite sending messages

--- productor-subscriber/receiver.rb
+++ productor-subscriber/receiver.rb
@@ -8,7 +8,10 @@ q = ch.queue("hello")
 
 puts " [x] Waiting for messages in #{q.name}. To exit press CTRL+C"
 q.subscribe(:block => true) do |delivery_info, properties, body|
+  # puts " [!] delivery info => #{delivery_info}"
+  # puts " [!] properties => #{properties}"
   puts " [x] Received #{body}"
 
+  # cancel the consumer to exit
   # delivery_info.consumer.cancel
 end
--- productor-subscriber/sender.rb
+++ productor-subscriber/sender.rb
@@ -1,7 +1,7 @@
 require 'bunny'
 
 # Creating conection with hostname called rabbit.local
-conn = Bunny.new
+conn = Bunny.new(:hostname => "localhost")
 conn.start
 
 # Channel where data resides
@@ -9,7 +9,17 @@ ch = conn.create_channel
 
 # Queue to publish messages
 q = ch.queue("hello")
-ch.default_exchange.publish("Hello World!", :routing_key => q.name)
-puts " [x] Sent Hello World!"
 
-conn.close
+
+while true do
+  puts "Write a message to send: "
+  message = gets.chomp
+  if message == "exit"
+    conn.close
+    break
+  end
+  ch.default_exchange.publish(message, :routing_key => q.name)
+  puts " [x] Sent #{message}"
+end
+
+
