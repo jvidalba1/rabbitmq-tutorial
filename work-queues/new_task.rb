cc7b658f9039ba78a7a7a2fc4ad56d58306e6c8b
Mateo Vidal <jvidalba@gmail.com>
2014-12-15 23:43:03 -0500

    work queues

--- work-queues/new_task.rb
+++ work-queues/new_task.rb
@@ -10,15 +10,9 @@ ch = conn.create_channel
 # Queue to publish messages
 q = ch.queue("hello")
 
-while true do
-  puts "Write a message to send: "
-  message = gets.chomp
-  if message == "exit"
-    conn.close
-    break
-  end
-  ch.default_exchange.publish(message, :routing_key => q.name)
-  puts " [x] Sent #{message}"
-end
+msg = ARGV.empty? ? "Hello World!" : ARGV.join(" ")
 
+q.publish(msg, :persistent => true)
+puts " [x] Sent #{msg}"
 
+conn.close
---
+++ work-queues/worker.rb
@@ -0,0 +1,15 @@
+require 'bunny'
+
+conn = Bunny.new
+conn.start
+
+ch = conn.create_channel
+q = ch.queue("hello")
+
+puts " [x] Waiting for messages in #{q.name}. To exit press CTRL+C"
+q.subscribe(:manual_ack => true, :block => true) do |delivery_info, properties, body|
+  puts " [x] Received #{body}"
+  sleep 1.0
+  puts " [x] Done"
+  ch.ack(delivery_info.delivery_tag)
+end
